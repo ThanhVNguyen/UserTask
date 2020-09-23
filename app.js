5b1933d03f53017b061cd62dfb2d655489bf5b25
DungTD10 <dungtd10@fpt.com.vn>
2020-09-23 09:47:44 +0700

    remove userTaskForm

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ const routes = require('./src/router');
 
 const app = express();
 app.use(bodyParer.json());
-app.use('/userTaskForm', routes.userTaskForm);
 app.use('/component', routes.component);
 app.use('/userTask', routes.userTask);
 app.use('/form', routes.form);
 
 const mongodb = 'mongodb://localhost:27017/UserTaskForm';
 mongoose.connect(mongodb, {
-  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false,
-},
-() => {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 app.listen(8000, () => {
-  console.log('Sever running is 8000:');
+  process.stdout.write('Sever running is 8000:');
 });
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,8 @@
-const userTaskForm = require('./userTaskForm');
 const userTask = require('./userTask');
 const component = require('./component');
 const form = require('./form');
 
 const routes = {
-  userTaskForm,
   userTask,
   component,
   form,
